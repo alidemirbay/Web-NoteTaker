4bc7291c9a4ca1d72de06226bbc962088473ec17
ademir <60299587+alidemirbay@users.noreply.github.com>
2020-12-18 11:16:27 -0800

    added comment

--- routes/htmlRoute.js
+++ routes/htmlRoute.js
@@ -1,7 +1,9 @@
-
+// We need to include the path package to get the correct file path for our html
 const path = require('path');
 
+// exports the routes
 module.exports = function (app) {
+    // In each of the below cases the user is shown an HTML page of content
     app.get("/notes", function (req, res) {
         res.sendFile(path.join(__dirname, "../public/notes.html"))
     });
