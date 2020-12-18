a92495d05689da6127e83ac5c26fa0c537566392
ademir <60299587+alidemirbay@users.noreply.github.com>
2020-12-18 11:56:22 -0800

    added comment

--- routes/apiRoute.js
+++ routes/apiRoute.js
@@ -1,23 +1,25 @@
 const fs = require("fs")
 
+// database which holds notes in array
 const db = require("../db/db.json")
 
-
+// id variable for deleting note
 let id = db.length + 1
 
 module.exports = function (app) {
 
+    // reads the database parse the content and send to the client
     app.get("/api/notes", function (req, res) {
-
         fs.readFile("./db/db.json", (err, data) => {
             if (err) throw err;
             res.json(JSON.parse(data))
         });
     })
 
+    // 
     app.post("/api/notes", function (req, res) {
+        // add id to the note,push into array wite to file and send to client
         req.body.id = id++;
-
         db.push(req.body)
         fs.writeFile("./db/db.json", JSON.stringify(db), function (err) {
             if (err) throw err
@@ -26,7 +28,8 @@ module.exports = function (app) {
     })
     app.delete("/api/notes/:id", function (req, res) {
         let getId = req.params.id
-
+        //find the note with correct id and remove it from array
+        // write to file and send to client
         for (let i = 0; i < db.length; i++) {
             if (db[i].id === parseInt(getId)) {
                 db.splice(i, 1);
