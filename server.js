b323465c0a11d3c3598322a0d7b2c4be73ee0d1f
ademir <60299587+alidemirbay@users.noreply.github.com>
2020-12-18 11:12:13 -0800

    added comment

--- server.js
+++ server.js
@@ -1,18 +1,24 @@
-
+//imports dependecies
 var express = require("express");
 
+//sets up express app
 var app = express();
+
 var PORT = process.env.PORT || 3000;
 
+// parses incoming requests
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+// serves static files
 app.use(express.static("public"));
 
 
-
+// immports html and api routes
 require('./routes/apiRoute')(app);
 require('./routes/htmlRoute')(app);
 
+//starts the server listening port
 app.listen(PORT, function () {
     console.log("App listening on PORT " + PORT);
 });
